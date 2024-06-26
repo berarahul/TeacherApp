a44d3540ab340cba037e096e44d4feead2a6b2f4
Rahul <rb6764385@gmail.com>
2024-06-26 23:29:56 +0530

    latest update

--- lib/view/Screens/BottomNavigation/Widgets/StudentTab/StudentDropdownWidgets/Widgets/student_tile.dart
+++ lib/view/Screens/BottomNavigation/Widgets/StudentTab/StudentDropdownWidgets/Widgets/student_tile.dart
@@ -1,19 +1,24 @@
 import 'package:attendence/res/Colors/AppColors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../../../../../view_model/services/Bottom_Navigation_Services/Bottom_Navigation_Controller.dart';
 import '../../../../../../../view_model/services/StudentTabServices/for_Student_list/controllers/studentTileController.dart';
+
+
+import '../../../../Bottom_Navigation_Menu.dart';
+import '../../../HomeTab/Home_Widget.dart'; // Adjust the import path as necessary
 
 class FetchingStudentScreen extends StatelessWidget {
   final StudentController studentController = Get.put(StudentController());
-
+  final BottomNavigationController bottomNavController = Get.find<BottomNavigationController>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
-          backgroundColor: AppColors.primaryColor,
-          automaticallyImplyLeading: false,
-          title: Text('Student List')),
+        backgroundColor: AppColors.primaryColor,
+        automaticallyImplyLeading: false,
+        title: Text('Student List'),
+      ),
       body: Obx(() {
         if (studentController.isLoading.value) {
           return Center(child: CircularProgressIndicator());
@@ -27,7 +32,7 @@ class FetchingStudentScreen extends StatelessWidget {
               var attendancePercentage = studentController
                   .calculateAttendancePercentage(student.attendance);
               var attendanceColor =
-                  studentController.getAttendanceColor(attendancePercentage);
+              studentController.getAttendanceColor(attendancePercentage);
 
               return studentTile(
                 name: student.name,
@@ -35,12 +40,20 @@ class FetchingStudentScreen extends StatelessWidget {
                 attendencePersent: attendancePercentage.toStringAsFixed(2),
                 attendanceColor: attendanceColor,
                 image:
-                    'https://via.placeholder.com/150', // Replace with actual image URL if available
+                'https://via.placeholder.com/150', // Replace with actual image URL if available
               );
             },
           );
         }
       }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          bottomNavController.selectedIndex.value = 0; // Set index to 0
+          Get.off(() => BottomNavigationMenu()); // Adjust the navigation as necessary
+        },
+        backgroundColor: AppColors.primaryColor,
+        child: Icon(Icons.home),
+      ),
     );
   }
 }
