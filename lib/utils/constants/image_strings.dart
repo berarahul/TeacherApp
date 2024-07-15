4333d0123ec9fef327af188d3cdb26e3ddb178dd
Rahul <rb6764385@gmail.com>
2024-07-15 22:22:00 +0530

    latest update

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,14 @@
+class RImages{
+
+  // -- App Logo
+  static const String darkAppLogo="assets/LOGOS/dark.png";
+  static const String LightAppLogo="assets/LOGOS/light.png";
+
+
+
+  // -- onBoarding Images
+  static const String onBoardingImage1="assets/images/onbordingimages/3rd.png";
+  static const String onBoardingImage2="assets/images/onbordingimages/3rd.png";
+  static const String onBoardingImage3="assets/images/onbordingimages/3rd.png";
+
+}
---
+++ lib/utils/constants/texts_String.dart
@@ -0,0 +1,29 @@
+class RTexts{
+
+  // -- GLOBAL TEXTS
+
+
+
+  // ONBOARDING TEXTS
+  static const String onBoardingTitle1="Manage your student attendance easily";
+  static const String onBoardingTitle2="Marking attendance is easy.";
+  static const String onBoardingTitle3="Track your student’s progress";
+
+
+  static const String onBoardingSubTitle1="Keep track of who’s present and who’s absent with ease. Efficiently manage attendance records for a smoother classroom flow.";
+  static const String onBoardingSubTitle2="Record attendance effortlessly with our clean and easy to use interface.";
+  static const String onBoardingSubTitle3="You can track your student’s attendance and their progress in the course. We also have a 75% quota feature that automatically marks students as completed if they meet the criteria.";
+
+  // HOME
+  static const String homeAppbarTitle="Good Day for Attendance";
+  static const String homeAppBarSubTitle="Rahul Bera";
+
+  // For Login
+  static const String loginTitle="Welcome";
+  static const String loginSubTitle="Hello sir, Please Login Your Account and take a students attendance";
+  static const String username="Username";
+  static const String password="Password";
+  static const String Forgetpassword="Forget Password?";
+  static const String Login="Login";
+
+}
